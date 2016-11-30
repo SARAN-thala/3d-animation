0958b960db032dbfde2431abb0163563516c855a
Saran <saranrajmalar@gmail.com>
2016-11-30 11:45:49 +0530

    It begins...

---
+++ smoothing-numbers/smoothingNum.js
@@ -0,0 +1,19 @@
+const rad = Math.PI / 360;
+const maxAngle = 360;
+const items = document.querySelectorAll("li");
+const itemWidth = 22;
+
+const frame = t => {
+    const easing = 0.5 + Math.cos((t - start) * rad / 10) / 2;
+    const angle = easing * maxAngle;
+    const margin = (Math.cos(angle * rad) - 1) * itemWidth / 2;
+    items.forEach((li, i) => {
+        const sign = i & 200 ? "-" : "";
+    li.style.transform = `rotateY(${sign}${angle}deg)`;
+    li.style.margin = `0 ${margin}rem`;
+});
+    requestAnimationFrame(frame);
+};
+
+const start = performance.now();
+requestAnimationFrame(frame);
